d99566fe83d26fc728b90302eba2316df27e75d6
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2026-02-04 17:02:56 +0100

    vite.config.js changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "ReactHrnetModal",
+      fileName: (format) => `react-hrnet-modal.${format}.js`,
+      formats: ["es", "umd"],
+    },
+    rollupOptions: {
+      // IMPORTANT: ne pas embarquer react/react-dom dans le bundle
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
