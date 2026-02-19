b72f6331d2b035c7fed39480d0ee5262f9b75e2f
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2026-02-19 13:27:55 +0000

    add className props to Modal component to enable style customization

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,6 +1,18 @@
 import React, { useEffect, useId } from "react";
 
-function Modal({ isOpen, onClose, children, title, closeOnOverlayClick = true, showCloseButton = true, closeOnEsc = true }) {
+function Modal({ 
+    isOpen, 
+    onClose, 
+    children, 
+    title, 
+    overlayClassName,
+    contentClassName,
+    titleClassName,
+    closeButtonClassName,
+    closeOnOverlayClick = true, 
+    showCloseButton = true, 
+    closeOnEsc = true 
+}) {
     const titleId = useId();
 
     // Lock Scroll
@@ -36,14 +48,14 @@ function Modal({ isOpen, onClose, children, title, closeOnOverlayClick = true, s
     if (!isOpen) return null; // Nothing to render if modal is close
 
     return (
-        <div className="hrnet-modal-overlay"  onClick={closeOnOverlayClick ? onClose : undefined} aria-modal="true" role="dialog" aria-labelledby={title ? titleId : undefined}>
-            <div className="hrnet-modal-content"  onClick={(e) => e.stopPropagation()}>
-                {title && <h2 id={titleId}>{title}</h2>}
+        <div className={`hrnet-modal-overlay ${overlayClassName ?? ""}`}  onClick={closeOnOverlayClick ? onClose : undefined} aria-modal="true" role="dialog" aria-labelledby={title ? titleId : undefined}>
+            <div className={`hrnet-modal-content ${contentClassName ?? ""}`}  onClick={(e) => e.stopPropagation()}>
+                {title && <h2 id={titleId} className={titleClassName}>{title}</h2>}
 
                 {children}
 
                 {showCloseButton && (
-                    <button type="button" className="hrnet-modal-close" onClick={onClose}>×</button>
+                    <button type="button" className={`hrnet-modal-close ${closeButtonClassName ?? ""}`} onClick={onClose}>×</button>
                 )}
 
             </div>
